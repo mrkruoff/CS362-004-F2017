2c4b65fe49554e065049693331b45fb9416185d0
kellandrosu <kellandr@oregonstate.edu>
2017-10-15 12:13:42 -0700

    impliment unittest3.c test on all cards and invalid input

--- kellandr/dominion/unittest3.c
+++ kellandr/dominion/unittest3.c
@@ -15,7 +15,7 @@
 
 int assertGameState(int player, int numPlayers, struct gameState *pre, struct gameState *post);
 
-void buyHelper(struct gameState *G, struct gameState *pre, int cardnum);
+void buyHelper(struct gameState *G, struct gameState *pre, int cardenum);
 
 
 int main (void){
@@ -35,7 +35,7 @@ int main (void){
 	testCount = failures = 0;
 
 	//initialize new game
-	initializeGame(numPlayers, k, seed, &G);	
+	initializeGame(numPlayers, k, seed, &G);
 	p = G.whoseTurn;
 
 	printf ("Testing buyCard():\n");
@@ -44,46 +44,54 @@ int main (void){
 	if(NOISY_TEST) 
 		printf("TESTING VALID INPUT\n");
 
-//try to buy with zero buys left should do nothing
+//try to buy with zero buys left (should do nothing)
 	if(NOISY_TEST) 
-		printf("Testing buyCard() with numBuys=0\n");
+		printf(" Testing buyCard() with numBuys = 0\n");
 	G.numBuys = 0;
+	G.phase = 1; //set phase to buy	
 	//copy pre gamestate
 	memcpy(&pre, &G, sizeof(struct gameState));
 	buyCard(adventurer, &G);
 	//test gamestate
-	failures = assertGameState(p, numPlayers, &pre, &G);
+	failures += assertGameState(p, numPlayers, &pre, &G);
 	testCount += 11 + 6 * numPlayers;
 
-//try to buy card that's too expensive - should do nothing
+//try to buy card that's too expensive (should do nothing)
+	G.phase = 1; //set phase to buy	
 	G.numBuys = 1;
 	G.coins = 4;
 	//copy pre gamestate
 	memcpy(&pre, &G, sizeof(struct gameState));
 	if(NOISY_TEST) 
-		printf("Testing buyCard() with numBuys= %d, coins= %d, price= %d\n", 
+		printf(" Testing buyCard() with numBuys = %d, coins = %d, price = %d\n", 
 				G.numBuys, G.coins, getCost(adventurer));
 
 	buyCard(adventurer, &G);
 	//test gamestate
-	failures = assertGameState(p, numPlayers, &pre, &G);
+	failures += assertGameState(p, numPlayers, &pre, &G);
 	testCount += 11 + 6 * numPlayers;
 
-//try to buy card with zero supply - should do nothing
+//try to buy card with zero supply (should do nothing)
+	G.phase = 1; //set phase to buy	
 	G.numBuys = 1;
 	G.coins = 6;
 	G.supplyCount[adventurer] = 0;
 	//copy pre gamestate
 	memcpy(&pre, &G, sizeof(struct gameState));
 	if(NOISY_TEST) 
-		printf("Testing buyCard() with supply= %d\n", G.supplyCount[adventurer]);
+		printf(" Testing buyCard() with supply = %d\n", G.supplyCount[adventurer]);
 	buyCard(adventurer, &G);
 	//test gamestate
-	failures = assertGameState(p, numPlayers, &pre, &G);
+	failures += assertGameState(p, numPlayers, &pre, &G);
 	testCount += 11 + 6 * numPlayers;
 
 
 //try to buy exact price and buys = should be successful
+	//initialize new game
+	initializeGame(numPlayers, k, seed, &G);
+	p = G.whoseTurn;
+
+	G.phase = 1; //set phase to buy	
 	G.numBuys = 1;
 	G.coins = getCost(adventurer);
 	//copy pre gamestate
@@ -92,39 +100,97 @@ int main (void){
 	buyHelper(&G, &pre, adventurer);
 	
 	//test gamestate
-	failures = assertGameState(p, numPlayers, &pre, &G);
+	failures += assertGameState(p, numPlayers, &pre, &G);
 	testCount += 11 + 6 * numPlayers;
 
 
-//buy with extra coins and extra buys
+//manually test buy with extra coins and extra buys
 	//initialize new game
 	initializeGame(numPlayers, k, seed, &G);	
 	p = G.whoseTurn;
-
+	G.phase = 1; //set phase to buy	
 	G.numBuys = 3;
 	G.coins = 10;
+
 	//copy pre gamestate
 	memcpy(&pre, &G, sizeof(struct gameState));
 	
 	buyHelper(&G, &pre, gardens);
 
 	//test gamestate
-	failures = assertGameState(p, numPlayers, &pre, &G);
+	failures += assertGameState(p, numPlayers, &pre, &G);
 	testCount += 11 + 6 * numPlayers;
 
 	buyHelper(&G, &pre, gold);
 
 	//test gamestate
-	failures = assertGameState(p, numPlayers, &pre, &G);
+	failures += assertGameState(p, numPlayers, &pre, &G);
 	testCount += 11 + 6 * numPlayers;
 
+//iterative test all valid card buys (set coins = buys = 100)
+	//initialize new game
+	initializeGame(numPlayers, k, seed, &G);	
+	G.phase = 1; //set phase to buy	
+	p = G.whoseTurn;
+	G.numBuys = G.coins = 100;
+
+	if(NOISY_TEST) 
+		printf("Testing buyCard() on all cards in k[]:\n");
+	//test valid kingdom cards;
+	for(int i=0; i<10; i++){
+		//copy pre gamestate
+		memcpy(&pre, &G, sizeof(struct gameState));
+		buyHelper(&G, &pre, k[i]);
+		//test gamestate
+		failures += assertGameState(p, numPlayers, &pre, &G);
+		testCount += 11 + 6 * numPlayers;
+	}
+	if(NOISY_TEST) 
+		printf("Testing buyCard() on all victory and coin cards:\n");
+	
+	for(int i = estate; i <= gold; i++){
+		//copy pre gamestate
+		memcpy(&pre, &G, sizeof(struct gameState));
+		buyHelper(&G, &pre, i);
+		//test gamestate
+		failures += assertGameState(p, numPlayers, &pre, &G);
+		testCount += 11 + 6 * numPlayers;
+	}
+
+
+
 //INVALID INPUT
-	//buy card not in game
+if(NOISY_TEST) 
+	printf("TESTING INVALID INPUT\n");
+	initializeGame(numPlayers, k, seed, &G);	
+	p = G.whoseTurn;
+	G.phase = 1; //set phase to buy	
+	G.numBuys = 1;
+	G.coins = 6;
 
-	//buy card not in array
+//buy card at illegal index (should do nothing)
+	//copy pre gamestate
+	memcpy(&pre, &G, sizeof(struct gameState));
+	if(NOISY_TEST) 
+		printf(" Testing buyCard() on illegal index\n");
+	buyCard(treasure_map+1, &G);
+	//test gamestate
+	failures += assertGameState(p, numPlayers, &pre, &G);
+	testCount += 11 + 6 * numPlayers;
 
 
-	printf("%d Total Tests: %d Failures, %d Passes\n\n", testCount, failures, testCount-failures);
+//buy card not in k[] array (should do nothing)
+	//copy pre gamestate
+	memcpy(&pre, &G, sizeof(struct gameState));
+	if(NOISY_TEST) 
+		printf(" Testing buyCard() on card not in k[]\n");
+	buyCard(salvager, &G);
+	//test gamestate
+	failures += assertGameState(p, numPlayers, &pre, &G);
+	testCount += 11 + 6 * numPlayers;
+
+
+	printf("%d Total Checks: %d Failures, %d Passes\n\n", testCount, failures, testCount-failures);
 
 	return 0;
 }
@@ -132,29 +198,28 @@ int main (void){
 
 //---------------------------     FUNCTIONS        --------------------------
 
-void buyHelper(struct gameState *G, struct gameState *pre, int cardnum){
+//buyHelper calls buyCard() for G and manually "buys" the card for the pre gameState
+void buyHelper(struct gameState *G, struct gameState *pre, int cardenum){
 	
 	if(NOISY_TEST) 
-		printf("Testing buyCard() with numBuys= %d, coins= %d, price= %d\n", 
-				G->numBuys, G->coins, getCost(cardnum));
+		printf(" Testing buyCard() with numBuys = %d, coins = %d, price = %d\n", 
+				G->numBuys, G->coins, getCost(cardenum));
 
-	//buy gardens
-	buyCard(cardnum, G);
+	//buy card for gamestate G
+	buyCard(cardenum, G);
 
-	pre->coins -= getCost(cardnum);
+	//manually "buy" card for pre gamestate
+	pre->coins -= getCost(cardenum);
 	pre->numBuys -= 1;
-
-	gainCard(cardnum, pre, 0, pre->whoseTurn);
+	gainCard(cardenum, pre, 0, pre->whoseTurn);
 	
-	if (pre->numBuys < 1)
-		pre->phase = 1;
 }
 
 //Helper Function: checks integer property of pre and post gameState
 int testStateInt(int prop1, int prop2, const char* name){
 	if (prop1 != prop2 ){
 		if (NOISY_TEST) 
-			printf(" FAIL: gameState.%s = %d expected %d\n", name, prop2, prop1 );
+			printf("  FAIL: gameState.%s = %d expected %d\n", name, prop2, prop1 );
 		return 1;
 	}
 	return 0;
@@ -165,7 +230,7 @@ int testStateInt(int prop1, int prop2, const char* name){
 int testStateArray(int a1[], int a2[], int size, const char* name){
 	if( memcmp(a1, a2, size) ){
 		if (NOISY_TEST) 
-			printf(" FAIL: gameState.%s array did not match expected\n", name);
+			printf("  FAIL: gameState.%s array did not match expected\n", name);
 		return 1;
 	}
 	return 0;
